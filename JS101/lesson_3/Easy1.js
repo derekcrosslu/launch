f798bd8275b5598d20f95a424e0ee7931ff48ae6
Derek Cross <derekcrosstest@gmail.com>
2023-10-20 12:16:10 -0500

    Leson 3, Practice Problems: Easy 1, Question 7

---
+++ JS101/lesson_3/Easy1.js
@@ -0,0 +1,77 @@
+// Question 1
+
+// Will the code below raise an error?
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+// this will not rise an error since it is creating the element with index 6 and value 5 during assignment operation
+//
+
+// Bonus:
+numbers[4]; // what will this line return? --- this will return undefined but not rise an error
+// correction...it does not have undefined even if it returns undefined, it has empty positions which shows when mapped
+// numbers.map(x=> typeof x)
+// [ 'number', 'number', 'number', <3 empty items>, 'number' ]
+// numbers[5] = undefined
+// [ 'number', 'number', 'number', <2 empty items>, 'undefined', 'number' ]
+
+// Question 2
+
+// How can you determine whether a given string ends with an exclamation mark (!)?
+
+let str1 = "Come over here!"; // true
+let str2 = "What's up, Doc?"; // false
+
+str1.indexOf("!") > -1; // true
+str2.indexOf("!") > -1; // false
+
+// Question 3
+
+// Determine whether the following object of people and their age contains an entry for 'Spot':
+
+// let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+ages.hasOwnProperty("Lily");
+
+// Question 4
+
+// Using the following string, create a new string that contains all lowercase letters except for the first character, which should be capitalized. (See the example output.)
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+munstersDescription[0]
+  .toUpperCase()
+  .concat(munstersDescription.slice(1).toLowerCase());
+// Solution 4
+munstersDescription.charAt(0).toUpperCase() +
+  munstersDescription.substring(1).toLowerCase();
+// my take on the solution
+munstersDescription
+  .charAt(0)
+  .toUpperCase()
+  .concat(munstersDescription.substring(1).toLowerCase());
+muns;
+munstersDescription.substring(munstersDescription.length - 9); // 'd Spooky.'
+
+// Question 5
+
+// What will the following code output?
+
+console.log(false == "0"); // true
+console.log(false === "0"); // false
+
+// Question 6
+
+// We have most of the Munster family in our ages object:
+
+let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+
+// Add entries for Marilyn and Spot to the object:
+let additionalAges = { Marilyn: 22, Spot: 237 };
+
+// first way
+ages = { ...ages, ...additionalAges };
+// second way (given solution)
+Object.assign(ages, additionalAges);
+
+// Question 7
+
+// Determine whether the name Dino appears in the strings below -- check each string separately:
