b3697265b9779d78dcfa16632414a71576e3eb54
Derek Cross <derekcrosstest@gmail.com>
2023-10-23 10:56:03 -0500

    Leson 3, Practice Problems: Medium 1, Question 2

--- JS101/lesson_3/Medium1.js
+++ JS101/lesson_3/Medium1.js
@@ -4,7 +4,23 @@
 
 // For this practice problem, write a program that outputs The Flintstones Rock! 10 times, with each line indented 1 space to the right of the line above it. The output should start out like this:
 
-let str = "The Flintstones Rock!";
-for (let i = 0; i < 10; i++) {
-  console.log(" ".repeat(i).concat(str));
-}
+// let str = "The Flintstones Rock!";
+// for (let i = 0; i < 10; i++) {
+//   console.log(" ".repeat(i).concat(str));
+// }
+
+// Question 2
+
+// Starting with the string:
+let munstersDescription = "The Munsters are creepy and spooky.";
+// Return a new string that swaps the case of all of the letters:
+`tHE mUNSTERS ARE CREEPY AND SPOOKY.`;
+
+console.log(
+  munstersDescription
+    .split("")
+    .map((char) =>
+      char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join("")
+);
